d22ceeb0d9dd6b05f083ac09b3a4d2cdaa614bd2
d.ivanov <d.ivanov@iac.spb.ru>
2022-09-28 17:05:41 +0600

    first commit

---
+++ lw.js
@@ -0,0 +1,55 @@
+"use strict"
+
+function _prepareReturnElement(element){
+    element.val = (value = null) => {
+        if(value)
+            element.value = value
+        return element.value 
+    }
+    element.text = (text = null) => {
+        if(text)
+            element.innerText = text
+        return element.text
+    }
+    element.html = (html = null) => {
+        if(html)
+            element.innerHTML = html
+        return element.html
+    }
+    return element
+}
+
+function lwID(id){
+    return _prepareReturnElement(document.getElementById(id))
+}
+
+function lwSelector(selector){
+    return _prepareReturnElement(document.querySelector(selector))
+}
+
+function lwSelectorAll(selector){
+    return _prepareReturnElement(document.querySelectorAll(selector))
+}
+
+function lwClass(_class){
+    return _prepareReturnElement(document.getElementsByClassName(_class))
+}
+
+function _lwCreateElement(nodeName, ID, classArray, parent){
+    let newElement = document.createElement(nodeName)
+    newElement.id = ID
+    classArray.forEach(e => {
+        newElement.classList.add(e);
+    })
+    if(parent){
+        if( typeof parent === 'string' )
+            parent = lwID(parent)
+        if(parent.appdenChild)
+            parent.appdenChild(newElement)
+    }
+    return _prepareReturnElement(newElement)
+}
+
+function lwDiv(ID, classArray = [], parent = null){
+    return _lwCreateElement('div', ID, classArray, parent)
+}
