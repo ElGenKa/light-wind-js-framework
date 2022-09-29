3c7b9f4fedeb30b0dda740d9a74d6cc477b4a3b5
d.ivanov <d.ivanov@iac.spb.ru>
2022-09-29 17:17:52 +0600

    js doc

--- lw.js
+++ lw.js
@@ -1,12 +1,18 @@
 "use strict"
 
-/*
-* @name Light Wind
-* @author Denis (ElGen) Ivanov
-* @version 0.1
-* @description dom library
-*/
+/**
+ * @name Light Wind
+ * @author Denis (ElGen) Ivanov
+ * @version 0.1
+ * @description dom library
+ * @git https://github.com/ElGenKa/light-wind-js-library
+ */
 
+/**
+ * Инициализирует дополнительный функционал элемента
+ * @param element
+ * @returns {HTMLElement}
+ */
 function lwPrepareElement(element) {
     if (!element)
         return element
@@ -68,28 +74,49 @@ function lwPrepareElement(element) {
         return element
     }
     element.hasClass = (className) => {
-        if (element.classList.contains(className))
-            return true
-        return false
+        return element.classList.contains(className);
+
     }
     element.isInitLW = true
     return element
 }
 
+/* Функции взаимодействия            */
+
 /* --------------------------------- */
 
+/**
+ * Возвращает элемент по его ID
+ * @param id {String}
+ * @returns {{object}|*}
+ */
 function lwID(id) {
     return lwPrepareElement(document.getElementById(id))
 }
 
+/**
+ * Возвращает элемент по селектору
+ * @param selector {String}
+ * @returns {HTMLElement}
+ */
 function lwSelector(selector) {
     return lwPrepareElement(document.querySelector(selector)[0])
 }
 
+/**
+ * Возвращает первый элемент по его имени
+ * @param name {String}
+ * @returns {HTMLElement}
+ */
 function lwName(name) {
     return lwPrepareElement(document.getElementsByName(name)[0])
 }
 
+/**
+ * Возвращает элементы по имени
+ * @param name {String}
+ * @returns {NodeListOf<HTMLElement>}
+ */
 function lwNames(name) {
     let elementsData = document.getElementsByName(name)
     for (let item of elementsData) {
@@ -98,6 +125,11 @@ function lwNames(name) {
     return elementsData
 }
 
+/**
+ *
+ * @param selector {String}
+ * @returns {NodeListOf<HTMLElement>}
+ */
 function lwSelectorAll(selector) {
     let elementsData = document.querySelectorAll(selector)
     for (let item of elementsData) {
@@ -106,10 +138,20 @@ function lwSelectorAll(selector) {
     return elementsData
 }
 
+/**
+ * Возвращает первый элемент по его имени тэга
+ * @param tag {String}
+ * @returns {HTMLElement}
+ */
 function lwTag(tag) {
     return lwPrepareElement(document.getElementsByTagName(tag)[0])
 }
 
+/**
+ * Возвращает элементы по имени
+ * @param tag {String}
+ * @returns {HTMLCollectionOf<HTMLElement>}
+ */
 function lwTags(tag) {
     let elementsData = document.getElementsByTagName(tag)
     for (let item of elementsData) {
@@ -118,13 +160,37 @@ function lwTags(tag) {
     return elementsData
 }
 
+/**
+ * Возвращает эементы по имени класса
+ * @param _class {String}
+ * @returns {HTMLCollectionOf<HTMLElement>}
+ */
 function lwClass(_class) {
-    return lwPrepareElement(document.getElementsByClassName(_class))
+    let elementsData = document.getElementsByClassName(_class)
+    for (let item of elementsData) {
+        lwPrepareElement(item)
+    }
+    return elementsData
 }
 
+/* Функции создания элементов        */
 /* --------------------------------- */
 
-function lwCreateElement(nodeName, ID, classArray, parent) {
+let lwElementsCounter = 0;
+
+/**
+ * Создает элемент, присваивает ему ID (если передан), добавляет классы и добавляет в переданный parent
+ * @param nodeName {String}
+ * @param ID {String|null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwCreateElement(nodeName, ID = null, classArray, parent) {
+    if (!ID)
+        ID = 'lwAutoID_' + lwElementsCounter + Math.floor(Math.random() * 999999999)
+
+
     let newElement = document.createElement(nodeName)
     newElement.id = ID
     classArray.forEach(e => {
@@ -139,132 +205,356 @@ function lwCreateElement(nodeName, ID, classArray, parent) {
     return lwPrepareElement(newElement)
 }
 
-function lwDiv(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwDiv(ID = null, classArray = [], parent = null) {
     return lwCreateElement('div', ID, classArray, parent)
 }
 
-function lwLabel(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwLabel(ID = null, classArray = [], parent = null) {
     return lwCreateElement('label', ID, classArray, parent)
 }
 
-function lwSmall(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwSmall(ID = null, classArray = [], parent = null) {
     return lwCreateElement('small', ID, classArray, parent)
 }
 
-function lwSpan(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwSpan(ID = null, classArray = [], parent = null) {
     return lwCreateElement('span', ID, classArray, parent)
 }
 
-function lwUl(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwUl(ID = null, classArray = [], parent = null) {
     return lwCreateElement('ul', ID, classArray, parent)
 }
 
-function lwImg(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwImg(ID = null, classArray = [], parent = null) {
     return lwCreateElement('img', ID, classArray, parent)
 }
 
-function lwLi(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwLi(ID = null, classArray = [], parent = null) {
     return lwCreateElement('li', ID, classArray, parent)
 }
 
-function lwOl(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwOl(ID = null, classArray = [], parent = null) {
     return lwCreateElement('ol', ID, classArray, parent)
 }
 
-function lwP(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwP(ID = null, classArray = [], parent = null) {
     return lwCreateElement('p', ID, classArray, parent)
 }
 
-function lwH1(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwH1(ID = null, classArray = [], parent = null) {
     return lwCreateElement('h1', ID, classArray, parent)
 }
 
-function lwH2(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwH2(ID = null, classArray = [], parent = null) {
     return lwCreateElement('h2', ID, classArray, parent)
 }
 
-function lwH3(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwH3(ID = null, classArray = [], parent = null) {
     return lwCreateElement('h3', ID, classArray, parent)
 }
 
-function lwH4(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwH4(ID = null, classArray = [], parent = null) {
     return lwCreateElement('h4', ID, classArray, parent)
 }
 
-function lwH5(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwH5(ID = null, classArray = [], parent = null) {
     return lwCreateElement('h5', ID, classArray, parent)
 }
 
-function lwH6(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwH6(ID = null, classArray = [], parent = null) {
     return lwCreateElement('h6', ID, classArray, parent)
 }
 
-function lwA(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwA(ID = null, classArray = [], parent = null) {
     return lwCreateElement('a', ID, classArray, parent)
 }
 
-function lwAddress(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwAddress(ID = null, classArray = [], parent = null) {
     return lwCreateElement('address', ID, classArray, parent)
 }
 
-function lwAbbr(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwAbbr(ID = null, classArray = [], parent = null) {
     return lwCreateElement('abbr', ID, classArray, parent)
 }
 
-function lwB(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwB(ID = null, classArray = [], parent = null) {
     return lwCreateElement('B', ID, classArray, parent)
 }
 
-function lwBr(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwBr(ID = null, classArray = [], parent = null) {
     return lwCreateElement('br', ID, classArray, parent)
 }
 
-function lwButton(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwButton(ID = null, classArray = [], parent = null) {
     return lwCreateElement('button', ID, classArray, parent)
 }
 
-function lwDel(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwDel(ID = null, classArray = [], parent = null) {
     return lwCreateElement('del', ID, classArray, parent)
 }
 
-function lwEm(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwEm(ID = null, classArray = [], parent = null) {
     return lwCreateElement('em', ID, classArray, parent)
 }
 
-function lwI(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwI(ID = null, classArray = [], parent = null) {
     return lwCreateElement('i', ID, classArray, parent)
 }
 
-function lwInput(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwInput(ID = null, classArray = [], parent = null) {
     return lwCreateElement('input', ID, classArray, parent)
 }
 
-function lwTextArea(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwTextArea(ID = null, classArray = [], parent = null) {
     return lwCreateElement('textarea', ID, classArray, parent)
 }
 
-function lwTable(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwTable(ID = null, classArray = [], parent = null) {
     return lwCreateElement('table', ID, classArray, parent)
 }
 
-function lwTBody(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwTBody(ID = null, classArray = [], parent = null) {
     return lwCreateElement('tbody', ID, classArray, parent)
 }
 
-function lwTHead(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwTHead(ID = null, classArray = [], parent = null) {
     return lwCreateElement('thead', ID, classArray, parent)
 }
 
-function lwTd(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwTd(ID = null, classArray = [], parent = null) {
     return lwCreateElement('td', ID, classArray, parent)
 }
 
-function lwTr(ID, classArray = [], parent = null) {
+/**
+ *
+ * @param ID {String|Null}
+ * @param classArray {Array}
+ * @param parent {String|HTMLElement}
+ * @returns {HTMLElement}
+ */
+function lwTr(ID = null, classArray = [], parent = null) {
     return lwCreateElement('tr', ID, classArray, parent)
 }
 
+/* Дополнительные фичи               */
 /* --------------------------------- */
 
+/**
+ * Скроллит к элементу. Принимает либо id либо эдемент
+ * @param element {{HTMLElement}|String}
+ * @param offset {Number}
+ * @param behavior {String}
+ */
 function lwScrollTo(element, offset = 140, behavior = 'smooth') {
     if (lwIsStr(element))
         element = lwID(element)
@@ -286,47 +576,81 @@ function lwScrollTo(element, offset = 140, behavior = 'smooth') {
     }
 }
 
-function lwIncludeJS(src){
+/**
+ * Подключает в head (или при его отсутствии в body) script
+ * @param src {String}
+ */
+function lwIncludeJS(src) {
     const htmlSctring = `/n<script src="${src}"></script>`
     _lwIncludeProcessor(htmlSctring)
 }
 
-function lwIncludeCSS(src){
+/**
+ * Подключает в head (или при его отсутствии в body) link
+ * @param src {String}
+ */
+function lwIncludeCSS(src) {
     const htmlSctring = `/n<link rel="stylesheet" href="${src}">`
     _lwIncludeProcessor(htmlSctring)
 }
 
-function _lwIncludeProcessor(html){
+function _lwIncludeProcessor(html) {
     const header = document.getElementsByTagName('head')
-    if(header.length > 0){
+    if (header.length > 0) {
         header[0].innerHTML += html
     } else {
         const body = document.getElementsByTagName('body')
-        if(body.length > 0)
+        if (body.length > 0)
             body[0].innerHTML += html
         else
             console.error('head and body not found')
     }
 }
 
+/* Функции проверок типов            */
 /* --------------------------------- */
 
+/**
+ * Проверяет на число
+ * @param data {*}
+ * @returns {boolean}
+ */
 function lwIsNum(data) {
     return typeof data === 'number'
 }
 
+/**
+ * Проверяет на Строку
+ * @param data {*}
+ * @returns {boolean}
+ */
 function lwIsStr(data) {
     return typeof data === 'string'
 }
 
+/**
+ * Проверяет на объект
+ * @param data {*}
+ * @returns {boolean}
+ */
 function lwIsObj(data) {
     return typeof data === 'object'
 }
 
+/**
+ * Проверяет на функцию
+ * @param data {*}
+ * @returns {boolean}
+ */
 function lwIsFunc(data) {
     return typeof data === 'function'
 }
 
+/**
+ * Проверяет на undefined
+ * @param data {*}
+ * @returns {boolean}
+ */
 function lwIsUnd(data) {
     return typeof data === 'undefined'
 }
