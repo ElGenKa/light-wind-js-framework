7e61314b8c40904eb36b6be545296fa894a057cf
d.ivanov <d.ivanov@iac.spb.ru>
2022-09-29 15:15:21 +0600

    fix and tests

--- lw.js
+++ lw.js
@@ -1,28 +1,31 @@
 "use strict"
 
 function _prepareReturnElement(element) {
-    if(!element)
+    if (!element)
         return element
 
-    if(element.isInitLW)
+    if (element.isInitLW)
+        return element
+
+    if (element.tagName === 'BODY' || element.tagName === 'HEAD' || element.tagName === 'HTML')
         return element
 
     element.val = (value = null) => {
-        if (value){
+        if (value) {
             element.value = value
             return element
         }
         return element.value
     }
     element.text = (text = null) => {
-        if (text){
+        if (text) {
             element.innerText = text
             return element
         }
         return element.text
     }
     element.html = (html = null) => {
-        if (html){
+        if (html) {
             element.innerHTML = html
             return element
         }
@@ -37,14 +40,14 @@ function _prepareReturnElement(element) {
         return element
     }
     element.attr = (attribute, value = null) => {
-        if(value){
+        if (value) {
             element.setAttribute(attribute, value)
             return element
         }
         return element.getAttribute(attribute)
     }
     element.attrs = (attributes) => {
-        for(let key in attributes){
+        for (let key in attributes) {
             element.attr(key, attributes[key])
         }
         return element
@@ -58,7 +61,7 @@ function _prepareReturnElement(element) {
         return element
     }
     element.hasClass = (className) => {
-        if(element.classList.contains(className))
+        if (element.classList.contains(className))
             return true
         return false
     }
@@ -76,11 +79,11 @@ function lwSelector(selector) {
     return _prepareReturnElement(document.querySelector(selector)[0])
 }
 
-function lwName(name){
+function lwName(name) {
     return _prepareReturnElement(document.getElementsByName(name)[0])
 }
 
-function lwNames(name){
+function lwNames(name) {
     let elementsData = document.getElementsByName(name)
     for (let item of elementsData) {
         _prepareReturnElement(item)
@@ -96,11 +99,11 @@ function lwSelectorAll(selector) {
     return elementsData
 }
 
-function lwTag(tag){
+function lwTag(tag) {
     return _prepareReturnElement(document.getElementsByTagName(tag)[0])
 }
 
-function lwTags(tag){
+function lwTags(tag) {
     let elementsData = document.getElementsByTagName(tag)
     for (let item of elementsData) {
         _prepareReturnElement(item)
@@ -118,7 +121,7 @@ function _lwCreateElement(nodeName, ID, classArray, parent) {
     let newElement = document.createElement(nodeName)
     newElement.id = ID
     classArray.forEach(e => {
-        newElement.classList.add(e);
+        newElement.classList.add(e)
     })
     if (parent) {
         if (lwIsStr(parent))
@@ -153,22 +156,46 @@ function lwP(ID, classArray = [], parent = null) {
     return _lwCreateElement('p', ID, classArray, parent)
 }
 
+function lwH1(ID, classArray = [], parent = null) {
+    return _lwCreateElement('h1', ID, classArray, parent)
+}
+
+function lwH2(ID, classArray = [], parent = null) {
+    return _lwCreateElement('h2', ID, classArray, parent)
+}
+
+function lwH3(ID, classArray = [], parent = null) {
+    return _lwCreateElement('h3', ID, classArray, parent)
+}
+
+function lwH4(ID, classArray = [], parent = null) {
+    return _lwCreateElement('h4', ID, classArray, parent)
+}
+
+function lwH5(ID, classArray = [], parent = null) {
+    return _lwCreateElement('h5', ID, classArray, parent)
+}
+
+function lwH6(ID, classArray = [], parent = null) {
+    return _lwCreateElement('h6', ID, classArray, parent)
+}
+
 /* --------------------------------- */
 
-function lwScrollTo(element, offset = 140, behavior = 'smooth'){
-    if(lwIsStr(element))
+function lwScrollTo(element, offset = 140, behavior = 'smooth') {
+    if (lwIsStr(element))
         element = lwID(element)
 
-    if(!lwIsNum(offset))
+    if (!lwIsNum(offset))
         offset = parseInt(offset)
 
     const typesBehavior = ['smooth', 'instant', 'auto']
-    if(typesBehavior.indexOf(behavior) === -1)
+    if (typesBehavior.indexOf(behavior) === -1)
         behavior = 'auto'
 
-    if(element){
-        const elementPosition = element.getBoundingClientRect().top;
-        const offsetPosition = elementPosition - offset;
+    if (element) {
+        const elementPosition = element.getBoundingClientRect().top
+        const offsetPosition = elementPosition - offset
         window.scrollBy({
             top: offsetPosition,
             behavior
@@ -176,24 +203,47 @@ function lwScrollTo(element, offset = 140, behavior = 'smooth'){
     }
 }
 
+function lwIncludeJS(src){
+    const htmlSctring = `/n<script src="${src}"></script>`
+    _lwIncludeProcessor(htmlSctring)
+}
+
+function lwIncludeCSS(src){
+    const htmlSctring = `/n<link rel="stylesheet" href="${src}">`
+    _lwIncludeProcessor(htmlSctring)
+}
+
+function _lwIncludeProcessor(html){
+    const header = document.getElementsByTagName('head')
+    if(header.length > 0){
+        header[0].innerHTML += html
+    } else {
+        const body = document.getElementsByTagName('body')
+        if(body.length > 0)
+            body[0].innerHTML += html
+        else
+            console.error('head and body not found')
+    }
+}
+
 /* --------------------------------- */
 
-function lwIsNum(data){
+function lwIsNum(data) {
     return typeof data === 'number'
 }
 
-function lwIsStr(data){
+function lwIsStr(data) {
     return typeof data === 'string'
 }
 
-function lwIsObj(data){
+function lwIsObj(data) {
     return typeof data === 'object'
 }
 
-function lwIsFunc(data){
+function lwIsFunc(data) {
     return typeof data === 'function'
 }
 
-function lwIsUnd(data){
+function lwIsUnd(data) {
     return typeof data === 'undefined'
 }
