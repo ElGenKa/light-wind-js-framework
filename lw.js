a6418e6de783d80e85227ccaa42546961ea84edc
d.ivanov <d.ivanov@iac.spb.ru>
2022-09-29 15:57:48 +0600

    add elements and refactoring

--- lw.js
+++ lw.js
@@ -1,6 +1,6 @@
 "use strict"
 
-function _prepareReturnElement(element) {
+function lwPrepareElement(element) {
     if (!element)
         return element
 
@@ -72,21 +72,21 @@ function _prepareReturnElement(element) {
 /* --------------------------------- */
 
 function lwID(id) {
-    return _prepareReturnElement(document.getElementById(id))
+    return lwPrepareElement(document.getElementById(id))
 }
 
 function lwSelector(selector) {
-    return _prepareReturnElement(document.querySelector(selector)[0])
+    return lwPrepareElement(document.querySelector(selector)[0])
 }
 
 function lwName(name) {
-    return _prepareReturnElement(document.getElementsByName(name)[0])
+    return lwPrepareElement(document.getElementsByName(name)[0])
 }
 
 function lwNames(name) {
     let elementsData = document.getElementsByName(name)
     for (let item of elementsData) {
-        _prepareReturnElement(item)
+        lwPrepareElement(item)
     }
     return elementsData
 }
@@ -94,30 +94,30 @@ function lwNames(name) {
 function lwSelectorAll(selector) {
     let elementsData = document.querySelectorAll(selector)
     for (let item of elementsData) {
-        _prepareReturnElement(item)
+        lwPrepareElement(item)
     }
     return elementsData
 }
 
 function lwTag(tag) {
-    return _prepareReturnElement(document.getElementsByTagName(tag)[0])
+    return lwPrepareElement(document.getElementsByTagName(tag)[0])
 }
 
 function lwTags(tag) {
     let elementsData = document.getElementsByTagName(tag)
     for (let item of elementsData) {
-        _prepareReturnElement(item)
+        lwPrepareElement(item)
     }
     return elementsData
 }
 
 function lwClass(_class) {
-    return _prepareReturnElement(document.getElementsByClassName(_class))
+    return lwPrepareElement(document.getElementsByClassName(_class))
 }
 
 /* --------------------------------- */
 
-function _lwCreateElement(nodeName, ID, classArray, parent) {
+function lwCreateElement(nodeName, ID, classArray, parent) {
     let newElement = document.createElement(nodeName)
     newElement.id = ID
     classArray.forEach(e => {
@@ -129,55 +129,131 @@ function _lwCreateElement(nodeName, ID, classArray, parent) {
         if (parent.appendChild)
             parent.appendChild(newElement)
     }
-    return _prepareReturnElement(newElement)
+    return lwPrepareElement(newElement)
 }
 
 function lwDiv(ID, classArray = [], parent = null) {
-    return _lwCreateElement('div', ID, classArray, parent)
+    return lwCreateElement('div', ID, classArray, parent)
 }
 
 function lwLabel(ID, classArray = [], parent = null) {
-    return _lwCreateElement('label', ID, classArray, parent)
+    return lwCreateElement('label', ID, classArray, parent)
 }
 
 function lwSmall(ID, classArray = [], parent = null) {
-    return _lwCreateElement('small', ID, classArray, parent)
+    return lwCreateElement('small', ID, classArray, parent)
+}
+
+function lwSpan(ID, classArray = [], parent = null) {
+    return lwCreateElement('span', ID, classArray, parent)
 }
 
 function lwUl(ID, classArray = [], parent = null) {
-    return _lwCreateElement('ul', ID, classArray, parent)
+    return lwCreateElement('ul', ID, classArray, parent)
+}
+
+function lwImg(ID, classArray = [], parent = null) {
+    return lwCreateElement('img', ID, classArray, parent)
 }
 
 function lwLi(ID, classArray = [], parent = null) {
-    return _lwCreateElement('li', ID, classArray, parent)
+    return lwCreateElement('li', ID, classArray, parent)
+}
+
+function lwOl(ID, classArray = [], parent = null) {
+    return lwCreateElement('ol', ID, classArray, parent)
 }
 
 function lwP(ID, classArray = [], parent = null) {
-    return _lwCreateElement('p', ID, classArray, parent)
+    return lwCreateElement('p', ID, classArray, parent)
 }
 
 function lwH1(ID, classArray = [], parent = null) {
-    return _lwCreateElement('h1', ID, classArray, parent)
+    return lwCreateElement('h1', ID, classArray, parent)
 }
 
 function lwH2(ID, classArray = [], parent = null) {
-    return _lwCreateElement('h2', ID, classArray, parent)
+    return lwCreateElement('h2', ID, classArray, parent)
 }
 
 function lwH3(ID, classArray = [], parent = null) {
-    return _lwCreateElement('h3', ID, classArray, parent)
+    return lwCreateElement('h3', ID, classArray, parent)
 }
 
 function lwH4(ID, classArray = [], parent = null) {
-    return _lwCreateElement('h4', ID, classArray, parent)
+    return lwCreateElement('h4', ID, classArray, parent)
 }
 
 function lwH5(ID, classArray = [], parent = null) {
-    return _lwCreateElement('h5', ID, classArray, parent)
+    return lwCreateElement('h5', ID, classArray, parent)
 }
 
 function lwH6(ID, classArray = [], parent = null) {
-    return _lwCreateElement('h6', ID, classArray, parent)
+    return lwCreateElement('h6', ID, classArray, parent)
+}
+
+function lwA(ID, classArray = [], parent = null) {
+    return lwCreateElement('a', ID, classArray, parent)
+}
+
+function lwAddress(ID, classArray = [], parent = null) {
+    return lwCreateElement('address', ID, classArray, parent)
+}
+
+function lwAbbr(ID, classArray = [], parent = null) {
+    return lwCreateElement('abbr', ID, classArray, parent)
+}
+
+function lwB(ID, classArray = [], parent = null) {
+    return lwCreateElement('B', ID, classArray, parent)
+}
+
+function lwBr(ID, classArray = [], parent = null) {
+    return lwCreateElement('br', ID, classArray, parent)
+}
+
+function lwButton(ID, classArray = [], parent = null) {
+    return lwCreateElement('button', ID, classArray, parent)
+}
+
+function lwDel(ID, classArray = [], parent = null) {
+    return lwCreateElement('del', ID, classArray, parent)
+}
+
+function lwEm(ID, classArray = [], parent = null) {
+    return lwCreateElement('em', ID, classArray, parent)
+}
+
+function lwI(ID, classArray = [], parent = null) {
+    return lwCreateElement('i', ID, classArray, parent)
+}
+
+function lwInput(ID, classArray = [], parent = null) {
+    return lwCreateElement('input', ID, classArray, parent)
+}
+
+function lwTextArea(ID, classArray = [], parent = null) {
+    return lwCreateElement('textarea', ID, classArray, parent)
+}
+
+function lwTable(ID, classArray = [], parent = null) {
+    return lwCreateElement('table', ID, classArray, parent)
+}
+
+function lwTBody(ID, classArray = [], parent = null) {
+    return lwCreateElement('tbody', ID, classArray, parent)
+}
+
+function lwTHead(ID, classArray = [], parent = null) {
+    return lwCreateElement('thead', ID, classArray, parent)
+}
+
+function lwTd(ID, classArray = [], parent = null) {
+    return lwCreateElement('td', ID, classArray, parent)
+}
+
+function lwTr(ID, classArray = [], parent = null) {
+    return lwCreateElement('tr', ID, classArray, parent)
 }
 
 /* --------------------------------- */
